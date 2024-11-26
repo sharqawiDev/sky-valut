08f89123fcb5677076c21014d7df923eda96605f
Mohammed <msharqawi707@gmail.com>
2024-11-26 17:48:45 +0300

    basic homepage

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -2,7 +2,14 @@ import { app, BrowserWindow } from "electron";
 import path from "path";
 import { isDev } from "./util.js";
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    resizable: false, // Disable resizing
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
   if (isDev()) mainWindow.loadURL("http://localhost:6600");
   else
     mainWindow.loadFile(path.join(app.getAppPath() + "/dist-react/index.html"));
--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -1,3 +1,13 @@
+import { auth } from "../constants";
+import "./App.scss";
 export const Home = () => {
-  return <div>Home</div>;
+  const { currentUser, name, signOut } = auth;
+  return (
+    <div className="home-page">
+      <div className="home-page__header">
+        <p>Welcome, user</p>
+        <button>Sign Out</button>
+      </div>
+    </div>
+  );
 };
